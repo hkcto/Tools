8292b3f9e715c2a8bb26ba3234f86f66dd6b44e1
Ko Lee <kay.lee@IW3E.LOCAL>
2019-06-03 13:34:39 +0800

    resize commi

--- tinypng.py
+++ tinypng.py
@@ -10,14 +10,25 @@ def tinc(source=None):
         chdir(source)
         types = ['*.jpg', '*.jpeg', '*.png']
         files = []
+
+
+
         for file in types:
             files.extend(glob(file))
         
         for i in files:
             print(i)
         
+        # images resize to 2880
+        for i in files:
+            source = tinify.from_file(i)
+            resized = source.resize(
+            method="scale",
+            width=2880)
+            resized.to_file(i)
+            print(i, 'resize to 2880 done')
+        
         print('compressioning...')
-
         for i in files:
             source = tinify.from_file(i)
             source.to_file(i)
